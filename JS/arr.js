73b10cca9280b535d6a8a70128514b154d73a206
M Nouman <rn9728959@gmail.com>
2025-12-06 20:14:37 +0500

    latest

---
+++ JS/arr.js
@@ -0,0 +1,24 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// push add at last
+// pop  removes from last
+// shift remove from start
+// unshift add at start
+
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+
+const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+
+let myList = fruits.toString();
+
+const arr = [1,2,3,4,5,6];
+let remEle = arr.shift()
+arr.unshift("Kiwi")
+
+let merged_arr = fruits2.concat(arr)
+
+const myArr = [[1,2],[3,4],[5,6]];
+const newArr = myArr.flat();
---
+++ JS/op.js
@@ -0,0 +1,29 @@
+// Arithmatic Operators
+
+let a = 4
+let b = 4
+let s = a + b
+let m = a - b
+let mul = a * b
+let d = a / b
+let mod = a % b
+
+// logical Operators
+// AND  (&&)
+// OR   (||)
+// NOT  (!)
+
+// Asssignment Operator
+// =
+let assignMenOP = 6
+
+// Equality Check
+// == , ===, !=
+
+
+// Relational Operators
+// > , < , >= , <=
+
+let num1 = 98
+let num2 = 34
+
